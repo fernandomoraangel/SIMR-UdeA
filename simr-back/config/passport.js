c445f27b7602bfeef45b0eac7b3ab8aa73eee8bc
Luis Velasquez <luis.velasquezg@udea.edu.co>
2024-07-09 09:28:32 -0500

    fix(Backend): :bug: Corrección del inicio de sesión
    Se corrigen errores de los archivos 'passport', 'local' (Passport Strategies), y 'express' para habilitar el inicio de sesión

--- simr-back/config/passport.js
+++ simr-back/config/passport.js
@@ -1,55 +1,10 @@
 'use strict';
 
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../app/models/user.server.model'); // Ajusta la ruta según tu estructura
-
-// passport.use(new LocalStrategy({
-//     usernameField: 'username',
-//     passwordField: 'password'
-// }, async (username, password, done) => {
-//     try {
-//         const user = await User.findOne({ username: username });
-//         if (!user) {
-//             return done(null, false, { message: 'Incorrect credentials.' });
-//         }
-//         const isMatch = await user.comparePassword(password);
-//         if (!isMatch) {
-//             return done(null, false, { message: 'Incorrect password.' });
-//         }
-//         return done(null, user);
-//     } catch (err) {
-//         return done(err);
-//     }
-// }));
-
-// passport.serializeUser((user, done) => {
-//     done(null, user.id);
-// });
-
-// passport.deserializeUser(async (id, done) => {
-//     try {
-//         const user = await User.findById(id);
-//         done(null, user);
-//     } catch (err) {
-//         done(err);
-//     }
-// });
-
-// module.exports = passport;
-
-
-
-
-// [Original code]
-
-// 'use strict';
-
-// var passport = require('passport');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 module.exports = function () {
-	var User = mongoose.model('User');
+	const User = mongoose.model('User');
 
 	// Definen cómo manejará passport la serialización de los usuarios
 	passport.serializeUser(function (user, done) {
@@ -58,14 +13,14 @@ module.exports = function () {
 
 	passport.deserializeUser(async function (id, done) {
 		try {
-			// Objeto opcion de mongoose para asegurar que no recupera esos campos
-			let user = await User.findOne({ _id: id }, '-password -salt');
+			const user = await User.findOne({ _id: id }).select('-password -salt').exec();
 			done(null, user);
 		} catch (err) {
-			done(err);
+			done(err, null);
 		}
 	});
 
 	require('./strategies/local.js')();
 	require('./strategies/google.js')();
 };
+
