f6666d83ee18092f7911513d2ffdfdb156a22f7b
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-06-22 23:28:39 -0500

    feat(Backend): :construction: [WIP] Sistema de Autenticación - Modificación de Rutas Protegidas
    Se modifica la lógica del middleware para rutas protegidas

--- simr-back/config/auth.js
+++ simr-back/config/auth.js
@@ -1,17 +1,17 @@
 // const passport = require('../config/passport');
 const passport = require("passport");
 
-// Middleware para rutas protegidas
+//* Middleware para rutas protegidas
 const requireAuth = (req, res, next) => {
-  passport.authenticate('jwt-access', { session: false }, (err, user, info) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err) {
       return res.status(500).json({ message: 'Error interno del servidor' });
     }
 
     if (!user) {
       return res.status(401).json({
-        message: 'Token requerido',
-        expired: info?.name === 'TokenExpiredError'
+        success: false,
+        message: 'Token de acceso requerido',
       });
     }
 
@@ -20,7 +20,18 @@ const requireAuth = (req, res, next) => {
   })(req, res, next);
 };
 
-// Middleware para verificar roles
+//* Middleware para rutas opcionales (si el usuario está autenticado, se agrega al req.user)
+// const optionalAuth = (req, res, next) => {
+//   passport.authenticate('jwt', { session: false }, (err, user, info) => {
+//     if (user) {
+//       req.user = user;
+//     }
+//     next();
+//   })(req, res, next);
+// };
+
+
+//* Middleware para verificar roles
 const requireRole = (roles) => {
   return (req, res, next) => {
     if (!req.user) {
