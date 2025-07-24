1b276cf1ef5a9f597fa9105a2228920505c9272c
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-07-24 18:53:35 -0500

    refactor: :fire: Elimina archivos innecesarios

--- simr-back/public/users/controllers/dashboard.client.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-angular.module("users")
-  .controller('DashboardController', function ($scope) {
-    // const user = JSON.parse(localStorage.getItem('user') || '{}');
-    // $scope.usuario = user;
-    return;
-  });
