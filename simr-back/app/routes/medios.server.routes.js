6060cff8c651a8727c9cb576dba61dc6d5caea40
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-08-04 01:15:33 -0500

    refactor(Backend): :recycle: Da formato al archivo

--- simr-back/app/routes/medios.server.routes.js
+++ simr-back/app/routes/medios.server.routes.js
@@ -2,22 +2,24 @@
 
 //Cargar dependencias
 
-var users=require('../../app/controllers/users.server.controller'),
-	medios=require('../../app/controllers/medios.server.controller');
+var users = require('../../app/controllers/users.server.controller'),
+  medios = require('../../app/controllers/medios.server.controller');
 
 //Definir el método routes del módulo
-module.exports=function(app){
-	//Configurar ruta base a 'obras'
-	app.route('/api/medios')
-	.get(medios.list)
-	.post(users.requiresLogin,medios.create);
+module.exports = function (app) {
+  //Configurar ruta base a 'obras'
+  app
+    .route('/api/medios')
+    .get(medios.list)
+    .post(users.requiresLogin, medios.create);
 
-	//Configurar las rutas a 'medios' parametrizadas
-	app.route('/api/medios/:medioId')
-	.get(medios.read)
-	.put(users.requiresLogin,medios.hasAuthorization,medios.update)
-	.delete(users.requiresLogin,medios.hasAuthorization,medios.delete);
+  //Configurar las rutas a 'medios' parametrizadas
+  app
+    .route('/api/medios/:medioId')
+    .get(medios.read)
+    .put(users.requiresLogin, medios.hasAuthorization, medios.update)
+    .delete(users.requiresLogin, medios.hasAuthorization, medios.delete);
 
-	//Configurar el parámetro middleware obraId
-	app.param('medioId',medios.medioByID);
+  //Configurar el parámetro middleware obraId
+  app.param('medioId', medios.medioByID);
 };
