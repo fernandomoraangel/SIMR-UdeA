d07866161b4c78b0a547af6a9b940ee23a76876f
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-08-04 23:22:09 -0500

    refactor(Backend): :recycle: Da formato al archivo

--- simr-back/app/routes/idiomas.server.routes.js
+++ simr-back/app/routes/idiomas.server.routes.js
@@ -2,22 +2,24 @@
 
 //Cargar dependencias
 
-var users=require('../controllers/users.server.controller'),
-	idiomas=require('../controllers/idiomas.server.controller');
+var users = require('../controllers/users.server.controller'),
+  idiomas = require('../controllers/idiomas.server.controller');
 
 //Definir el método routes del módulo
-module.exports=function(app){
-	//Configurar ruta base 
-	app.route('/api/idiomas')
-	.get(idiomas.list)
-	.post(users.requiresLogin,idiomas.create);
+module.exports = function (app) {
+  //Configurar ruta base
+  app
+    .route('/api/idiomas')
+    .get(idiomas.list)
+    .post(users.requiresLogin, idiomas.create);
 
-	//Configurar las rutas a  parametrizadas
-	app.route('/api/idiomas/:idiomaId')
-	.get(idiomas.read)
-	.put(users.requiresLogin,idiomas.hasAuthorization,idiomas.update)
-	.delete(users.requiresLogin,idiomas.hasAuthorization,idiomas.delete);
+  //Configurar las rutas a  parametrizadas
+  app
+    .route('/api/idiomas/:idiomaId')
+    .get(idiomas.read)
+    .put(users.requiresLogin, idiomas.hasAuthorization, idiomas.update)
+    .delete(users.requiresLogin, idiomas.hasAuthorization, idiomas.delete);
 
-	//Configurar el parámetro middleware
-	app.param('idiomaId',idiomas.idiomaByID);
+  //Configurar el parámetro middleware
+  app.param('idiomaId', idiomas.idiomaByID);
 };
