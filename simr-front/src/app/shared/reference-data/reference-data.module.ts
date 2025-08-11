a1e50ad50a45484db9297622dfbfb8b7b1d962e3
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-08-10 23:55:09 -0500

    feat(Frontend): :sparkles: Agrega módulo para permitir compartir servicios entre entidades

---
+++ simr-front/src/app/shared/reference-data/reference-data.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+  providers: [
+    // ActorService,
+    // InstrumentoService,
+    // IdiomaService,
+    // ColeccionService
+  ]
+})
+export class ReferenceDataModule { }
+
+// En cada módulo de entidad que necesite referencias
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     ReactiveFormsModule,
+//     ReferenceDataModule, // Import para acceder a servicios de referencia
+//     ObraRoutingModule
+//   ],
+//   declarations: [
+//     ObraListComponent,
+//     ObraFormComponent,
+//     ObraDetailComponent,
+//     ObraEditComponent
+//   ]
+// })
+// export class ObraModule { }
