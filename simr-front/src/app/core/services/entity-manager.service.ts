6de4233c9ca8a5a4d8b9114b41e67ae4aecfcdd8
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-08-10 23:52:38 -0500

    feat(Frontend):

---
+++ simr-front/src/app/core/services/entity-manager.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntityManagerService {
+//   private actorService = inject(ActorService);
+//   private instrumentoService = inject(InstrumentoService);
+//   private idiomaService = inject(IdiomaService);
+
+//   // Métodos para obtener datos de referencia
+//   getActores() {
+//     return this.actorService.getAll();
+//   }
+
+//   getInstrumentos() {
+//     return this.instrumentoService.getAll();
+//   }
+
+//   getIdiomas() {
+//     return this.idiomaService.getAll();
+//   }
+}
