03e6e151db7a1bdd45413191db6a9824dafc0cac
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-09-25 01:14:39 -0500

    refactor(Frontend): :recycle: Actualiza rutas de la aplicación

--- simr-front/src/app/app-routing.module.ts
+++ simr-front/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 // Componentes
-import { HomeComponent } from './pages/home/home.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './features/home/home.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { AuthGuard } from './core/auth/auth.guard';
+import { ListaTareasComponent } from '@features/__pruebas__/lista-tareas.component';
+// import { CustomPreloadingStrategy } from './core/services/preloading-strategy.service';
 // import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
@@ -14,42 +16,46 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'listado-tareas', component: ListaTareasComponent },
 
   // Lazy loaded routes
   {
     path: 'dashboard',
     loadChildren: () =>
-      import('./modules/dashboard/dashboard.module').then(
+      import('./features/dashboard/dashboard.module').then(
         (m) => m.DashboardModule
       ),
     canActivate: [AuthGuard],
   },
-  {
-    path: 'diccionarios',
-    loadChildren: () =>
-      import('./features/diccionario/diccionario.routes').then(
-        (r) => r.DICCIONARIOS_ROUTES
-      ),
-  },
+  // {
+  //   path: 'diccionarios',
+  //   loadChildren: () =>
+  //     import('./features/diccionarios/diccionarios.routes').then(
+  //       (r) => r.DICCIONARIOS_ROUTES
+  //     ),
+  // },
   // {
   //   path: 'diccionarios',
   //   loadChildren: () => import('./features/diccionarios/diccionario.module').then(m => m.DiccionariosModule)
   // },
   {
+    path: 'diccionarios',
+    loadChildren: () =>
+      import('./features/diccionarios/diccionarios.routes').then(
+        (r) => r.DICCIONARIOS_ROUTES
+      ),
+  },
+  {
     path: 'idiomas',
     loadChildren: () =>
-      import('./features/idioma/idioma.routes').then((r) => r.IDIOMAS_ROUTES),
+      import('./features/idiomas/idiomas.routes').then((r) => r.IDIOMAS_ROUTES),
   },
   {
     path: 'files',
     loadChildren: () =>
-      import('./modules/archivo/archivo.module').then((m) => m.ArchivoModule),
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'prueba',
-    loadChildren: () =>
-      import('./modules/prueba/prueba.module').then((m) => m.PruebaModule),
+      import('./features/archivos/archivos.module').then(
+        (m) => m.ArchivoModule
+      ),
     canActivate: [AuthGuard],
   },
 
@@ -62,8 +68,10 @@ const routes: Routes = [
       // Optimizaciones adicionales
       enableTracing: false, // solo para desarrollo
       preloadingStrategy: PreloadAllModules, // precarga módulos después de la carga inicial
+      // preloadingStrategy: CustomPreloadingStrategy, // precarga personalizada
     }),
   ],
   exports: [RouterModule],
+  // providers: [CustomPreloadingStrategy] // Para que Angular inyecte la estrategia de precarga personalizada
 })
 export class AppRoutingModule {}
