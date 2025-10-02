6795d1c677610844d03036cf3a585b2acf0857bb
fernandomoraangel <fernando.mora@udea.edu.co>
2025-10-02 11:33:50 -0500

    Configurar URLs para producción con IP 172.23.0.97 y variables de entorno para Docker

--- simr-back/config/env/production.js
+++ simr-back/config/env/production.js
@@ -3,11 +3,11 @@ module.exports = {
   // Si no es local mongodb://usuario:pass@hostname:puerto/basedatos
   // db: "mongodb://superAdmin:SOh3TbYhx8ypJPxmt1oOfL@simr-mongo:27017/simr",
   db: process.env.MONGO_URI,
-  sessionSecret: 'productionSessionSecret',
+  sessionSecret: "productionSessionSecret",
   google: {
     clientID:
-      '424952915433-7ejq1nho03771k9nc8rmdqmasqjfsqr7.apps.googleusercontent.com',
-    clientSecret: 'S0l1DWc4y0NMYSpDmyrxy3kL',
-    callbackURL: 'http://localhost:3000/oauth/google/callback',
+      "424952915433-7ejq1nho03771k9nc8rmdqmasqjfsqr7.apps.googleusercontent.com",
+    clientSecret: "S0l1DWc4y0NMYSpDmyrxy3kL",
+    callbackURL: "http://172.23.0.97/oauth/google/callback",
   },
 };
