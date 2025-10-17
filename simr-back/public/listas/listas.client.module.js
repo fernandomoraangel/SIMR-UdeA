bef0602c2050eac4b2a764fda2f21fe17b8bfd43
fernandomoraangel <fernando.mora@udea.edu.co>
2025-10-17 11:49:11 -0500

    style: :lipstick: Correcciones en estilos, colores y capitalización

--- simr-back/public/listas/listas.client.module.js
+++ simr-back/public/listas/listas.client.module.js
@@ -1,1 +1,8 @@
 angular.module("listas", []);
+// Filtro para mayúscula inicial
+angular.module("listas").filter("capitalize", function () {
+  return function (input) {
+    if (!input) return "";
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  };
+});
